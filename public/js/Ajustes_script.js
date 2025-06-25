f602b2bfbe13c4a7b5810bb4f09fb2cc83c83c25
Elian <u192168195@gmail.com>
2025-06-25 14:22:09 -0300

    Agregúe cambios en la scroll bar en su tamaño y color en el estilo global, También se solucionó el problema de elegir 5 jugadores (ahora es posible elegir 5) y se agregó un contenedor con overflow auto para cuando los elementos se salen del contenedor

--- public/js/Ajustes_script.js
+++ public/js/Ajustes_script.js
@@ -5,19 +5,9 @@ window.addEventListener("DOMContentLoaded", () => {
   const bloque_2 = document.getElementById("contenedor_02");
   const bloque_3 = document.getElementById("contenedor_03");
   const bloque_4 = document.getElementById("contenedor_04");
-  const admin = document.getElementById("admin");
-  const p1 = document.getElementById("p1");
-  const p2 = document.getElementById("p2");
-  const p3 = document.getElementById("p3");
-  const boton = document.getElementById("btn-verificar");
-  const valor_input_admin = document.getElementById("input-admin");
-  const valor_input_p2 = document.getElementById("input-p2");
-  const valor_input_p3 = document.getElementById("input-p3");
-  const valor_input_p4 = document.getElementById("input-p4");
+  const bloque_5 = document.getElementById("contenedor_05");
   bloque_1.style.display = "flex";
   bloque_2.style.display = "flex";
-  admin.style.display = "flex";
-  p1.style.display = "flex";
   selectElement.addEventListener("change", (event) => {
     // 'event' es el objeto evento que contiene información sobre el evento
     // 'event.target' se refiere al elemento select
@@ -28,25 +18,34 @@ window.addEventListener("DOMContentLoaded", () => {
       bloque_2.style.display = "flex";
       bloque_3.style.display = "none";
       bloque_4.style.display = "none";
-      p2.style.display = "none";
-      p3.style.display = "none";
+      bloque_5.style.display = "none";
     } else if (jugadores == 3) {
       bloque_1.style.display = "flex";
       bloque_2.style.display = "flex";
       bloque_3.style.display = "flex";
       bloque_4.style.display = "none";
-      p2.style.display = "flex";
-      p3.style.display = "none";
+      bloque_5.style.display = "none";
     } else if (jugadores == 4) {
       bloque_1.style.display = "flex";
       bloque_2.style.display = "flex";
       bloque_3.style.display = "flex";
       bloque_4.style.display = "flex";
-      p2.style.display = "flex";
-      p3.style.display = "flex";
+      bloque_5.style.display = "none";
+    } else if (jugadores == 5) {
+      bloque_1.style.display = "flex";
+      bloque_2.style.display = "flex";
+      bloque_3.style.display = "flex";
+      bloque_4.style.display = "flex";
+      bloque_5.style.display = "flex";
     }
   });
-  const jugadoresRegistrados = ["admin123", "player2", "player3", "player4"];
+  const jugadoresRegistrados = [
+    "admin123",
+    "player2",
+    "player3",
+    "player4",
+    "player5",
+  ];
 
   // Relación entre los inputs y los elementos visuales donde se muestra si están verificados o no
   const jugadores = [
@@ -66,6 +65,10 @@ window.addEventListener("DOMContentLoaded", () => {
     {
       input: document.getElementById("input-p4"),
       icono: document.getElementById("_p4"),
+    },
+    {
+      input: document.getElementById("input-p5"),
+      icono: document.getElementById("_p5"),
     },
   ];
 
