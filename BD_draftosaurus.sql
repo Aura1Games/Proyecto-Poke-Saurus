33797fd38307903f077cee300357a4a66576e507
Elian-zzz <u192168195ser@gmail.com>
2025-11-06 13:27:19 -0300

    fin operativo bd actualizada y database.php sin contra

--- BD_draftosaurus.sql
+++ BD_draftosaurus.sql
@@ -53,14 +53,8 @@ CREATE TABLE Tablero(
     FOREIGN KEY (id_usuario) REFERENCES Usuario(id_usuario)
 );
 
-#DELETE FROM Tablero where id_tablero >0;
-#ALTER TABLE Dinosaurio AUTO_INCREMENT = 1;
-
 # Consultar la tabla Tablero
 SELECT * FROM Tablero;
-
-# Ingresar datos a la tabla Tablero
-#INSERT INTO Tablero(id_partida) VALUES (1);
 
 
 # ================================
@@ -72,16 +66,8 @@ CREATE TABLE Recinto(
     id_tablero INT NOT NULL,
     FOREIGN KEY (id_tablero) REFERENCES Tablero(id_tablero)
 );
-
-# eliminar datos anteriores de la tabla
-#DELETE FROM Recinto where id_recinto >0;
-#ALTER TABLE Recinto AUTO_INCREMENT = 1;
-
 # Consultar la tabla Recinto
 SELECT * FROM Recinto;
-# Ingresar datos a la tabla Recinto
-#INSERT INTO Recinto(nombre, id_tablero) VALUES ("El Bosque de la Semejanza", 2),("El Prado de la Diferencia",2),("La Pradera del Amor",2),("El Trío Frondoso",2),("El Rey de la Selva",2),("La Isla Solitaria",2),("El Rio",2);
-
 
 # ================================
 # Tabla de dinosaurios: Dinosaurio(id_dinosaurio, especie)
@@ -111,71 +97,24 @@ CREATE TABLE Juega(
     FOREIGN KEY (id_partida) REFERENCES Partida(id_partida)
 );
 
-# eliminar datos existentes de la tabla
-#DELETE FROM Juega where id_usuario >0;
-
 # Consultar la tabla Juega
 SELECT * FROM Juega;
-#select id_partida from Juega order by id_partida desc limit 1;
-
-# Ingresar datos a la tabla Juega
-# INSERT INTO Juega(id_usuario, id_partida) VALUES (2, 7);
-
-# ================================
-# Relación Recinto - Dinosaurio: Colocacion(id_recinto, id_dinosaurio)
-# ================================
-CREATE TABLE Colocacion(
-    id_recinto INT NOT NULL,
-    id_dinosaurio INT NOT NULL,
-    id_tablero INT NOT NULL,
-    PRIMARY KEY (id_recinto, id_tablero),
-    FOREIGN KEY (id_recinto) REFERENCES Recinto(id_recinto),
-    FOREIGN KEY (id_dinosaurio) REFERENCES Dinosaurio(id_dinosaurio),
-    FOREIGN KEY (id_tablero) REFERENCES Tablero(id_tablero)
-);
-
-
-
-# Ingresar datos a la tabla Colocacion
-INSERT INTO Colocacion(id_recinto,id_dinosaurio) values (2,1);
-
-# Eliminar los datos previos de la tabla Colocacion y reiniciar auto_increment
-#DELETE FROM Colocacion where id_recinto >0;
-#ALTER TABLE Dinosaurio AUTO_INCREMENT = 1;
-
-# Consultar la tabla Colocacion
-SELECT * FROM Colocacion ;
-
 
 
 # Consultar de ranking
-SELECT  
-    u.nombre AS Nombre,
-    u.edad AS Edad,
-    p.puntaje_final AS Puntos,
-    CASE 
-        WHEN p.estado = 1 THEN 'Completada'
-        WHEN p.estado = 0 THEN 'Incompleta'
-        ELSE 'Desconocido'
-    END AS Estado
-FROM Partida p
-JOIN Usuario u ON p.ganador = u.id_usuario
-WHERE p.puntaje_final IS NOT NULL and Estado is not FALSE
-ORDER BY p.puntaje_final DESC LIMIT 7;
-
-# Obtener el estado de una partida
-select estado from Partida where id_partida = 1; 
-
-select * from Partida;
-
-# Cambiar el estado de la partida
-UPDATE Partida
-SET estado = TRUE, puntaje_final = 35
-WHERE id_partida = 1;
-
-SELECT * FROM Partida;
-
-
+#SELECT  
+#    u.nombre AS Nombre,
+#    u.edad AS Edad,
+#    p.puntaje_final AS Puntos,
+#    CASE 
+#        WHEN p.estado = 1 THEN 'Completada'
+#        WHEN p.estado = 0 THEN 'Incompleta'
+#        ELSE 'Desconocido'
+#    END AS Estado
+#FROM Partida p
+#JOIN Usuario u ON p.ganador = u.id_usuario
+#WHERE p.puntaje_final IS NOT NULL and Estado is not FALSE
+#ORDER BY p.puntaje_final DESC LIMIT 7;
 
 
 
